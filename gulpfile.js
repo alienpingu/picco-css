6a60900964a51d467f01b548db6062534d65f58b
Samuele Bertocco <samuele.bertocco@gmail.com>
2025-03-20 17:47:44 +0100

    refactor styles and update build process for consistency

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const cssnano = require('gulp-cssnano');
 // Definisci il task per concatenare e minificare i CSS
 gulp.task('styles', () => {
     return gulp.src('src/style/*.css') // Prendi tutti i file CSS nella cartella src/style/
-        .pipe(concat('styles.min.css'))   // Concatenate tutti i CSS in un unico file styles.min.css
+        .pipe(concat('picco.min.css'))   // Concatenate tutti i CSS in un unico file styles.min.css
         .pipe(cssnano())                 // Minifica il CSS
         .pipe(gulp.dest('dist/'));        // Salva il file minificato nella cartella dist/
 });
